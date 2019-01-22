4b38903a1debadfef8ccc38f7e9434bad2d35b3e
Tim Cunningham <timothypaulcunningham@gmail.com>
2019-01-22 17:58:19 -0500

    Add grid lines to make tiles easier to see

--- js/tile.js
+++ js/tile.js
@@ -8,6 +8,12 @@ class Tile {
 
     draw(x, y) {
         image(this.img, x*this.size, y*this.size, this.size, this.size, this.imgX, this.imgY, this.size, this.size);
+        push();
+        noFill();
+        strokeWeight(2);
+        stroke(51);
+        rect(x*this.size, y*this.size, this.size, this.size);
+        pop();
     }
 
     copy() {
